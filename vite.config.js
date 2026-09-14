965e13b5fecedc3892036d799386200b730b409a
Gamal Badie <149597937+bd-gamal@users.noreply.github.com>
2026-09-14 16:54:08 +0100

    feat: update package dependencies and configure WebSocket proxy in Vite

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,17 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+      "/ws": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+  },
 });
